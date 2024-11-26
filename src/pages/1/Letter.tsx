ddf229fc21ce275ccf2f1e8bce4b0a76b5b234a4
andimian <andimian@bk.ru>
2024-11-26 22:45:31 +0500

    Добавляет цифры календаря

--- src/pages/1/Letter.tsx
+++ src/pages/1/Letter.tsx
@@ -1,14 +1,63 @@
 import { FC } from 'react';
 import letterImg from './assets/images/letterImg.jpg';
 import letterImg2 from './assets/images/letterImg2.jpg';
-// import PresentImg from '../../assets/images/present.svg';
+import helloNumberImg from '../../assets/images/0.jpg';
+import timeImg from '../../assets/images/present.svg';
 
 export const Letter: FC = () => {
 		return (
 			<div>
+				<ul className="lightrope">
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+				<div className='numbersWrapper'>
+					<img src={timeImg} alt="Дата"/>
+					<img src={helloNumberImg} alt="Дата"/>
+				</div>
 				<div className='imagesWrapper'>
-					<img src={letterImg} alt="Письмо деду морозу"/>
 					<img src={letterImg2} alt="Письмо деду морозу"/>
+					<img src={letterImg} alt="Письмо деду морозу"/>
 				</div>
 				<div className="title">
 					{/*<PresentImg/>*/}

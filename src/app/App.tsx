5650c2e93f85f4f20b2ca3c1b639260235b7efa2
andimian <andimian@bk.ru>
2024-11-27 19:44:40 +0500

    Добавляет ветку gh-pages

---
+++ src/app/App.tsx
@@ -0,0 +1,24 @@
+import css from './app.module.scss';
+import '../assets/styles/base.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Letter } from '../pages/1/Letter';
+import { Garland } from '../features/garland/Garland';
+import treeImg from '../assets/images/tree.jpg';
+
+function App() {
+
+	return (<div className={css.wrapper}>
+			<Garland/>
+			<BrowserRouter basename="/advent-calendar">
+				<Routes>
+					<Route
+						path="/1"
+						element={<Letter/>}
+					/>
+				</Routes>
+			</BrowserRouter>
+			<div className='endImg'><img src={treeImg} alt="Дата"/></div>
+		</div>)
+}
+
+export default App

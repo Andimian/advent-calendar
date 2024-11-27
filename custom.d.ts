f314974ccb49ba6ebe6373ef967555f63e78f080
sokolov_anton <andimiangm@gmail.com>
2024-11-27 16:50:43 +0500

    Добавляет настройку для загрузки на gh-pages

--- custom.d.ts
+++
@@ -1,6 +0,0 @@
-// custom.d.ts или svg.d.ts
-declare module '*.svg' {
-	import { ReactElement } from 'react';
-	const content: ReactElement;
-	export default content;
-}
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import css from './app.module.scss';
 import '../assets/styles/base.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Letter } from '../pages/1/Letter.tsx';
-import { Garland } from '../features/garland/Garland.tsx';
+import { Letter } from '../pages/1/Letter';
+import { Garland } from '../features/garland/Garland';
 import treeImg from '../assets/images/tree.jpg';
 
 function App() {

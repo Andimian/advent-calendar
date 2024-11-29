d907d85e661ccc80207d473539e15070bde11b04
sokolov_anton <andimiangm@gmail.com>
2024-11-29 10:53:41 +0500

    Добавляет компонент кнопки для прослушивания аудио, обновляет стили

---
+++ src/features/audioPlayer/AudioPlayer.tsx
@@ -0,0 +1,35 @@
+import { useRef, useState } from 'react';
+import styles from './styles.module.scss';
+import dedImg from '../../assets/images/ded.png';
+
+export const AudioPlayer: React.FC = () => {
+	// Используем useRef для создания ссылки на элемент audio
+	const audioRef = useRef<HTMLAudioElement | null>(null);
+	const [isPlaying, setIsPlaying] = useState(false);
+
+	// Функция для воспроизведения/паузы
+	const togglePlayPause = () => {
+		if (audioRef.current) {
+			if (isPlaying) {
+				audioRef.current.pause(); // Пауза
+			} else {
+				audioRef.current.play();  // Воспроизведение
+			}
+			setIsPlaying(!isPlaying);  // Меняем состояние кнопки
+		}
+	};
+
+	// Обработчик для события окончания трека
+	const handleTrackEnd = () => {
+		setIsPlaying(false); // Когда трек заканчивается, сбрасываем состояние кнопки
+	};
+
+	return (
+		<div>
+			<button onClick={togglePlayPause} className={styles.btn}>
+				<img src={dedImg} className={styles.dedImg} alt=""/>
+				{isPlaying ? 'Остановить' : 'Послушать'}
+			</button>
+			<audio ref={audioRef} src="public/assets/audio/hello.mp3" onEnded={handleTrackEnd} />
+		</div>);
+};
--- src/pages/1/Letter.tsx
+++ src/pages/1/Letter.tsx
@@ -3,32 +3,36 @@ import letterImg from './assets/images/letterImg.jpg';
 import letterImg2 from './assets/images/letterImg2.jpg';
 import helloNumberImg from '../../assets/images/1.jpeg';
 import timeImg from '../../assets/images/present.svg';
+import { AudioPlayer } from '../../features/audioPlayer/AudioPlayer';
 
 export const Letter: FC = () => {
-		return (
-			<div className='container'>
-				<div className='numbersWrapper'>
-					<div><img src={timeImg} alt="Дата"/></div>
-					<div><img src={helloNumberImg} alt="Дата"/></div>
-				</div>
-				<div className="title">
-					{/*<PresentImg/>*/}
-					<h1>
-						Пишем письмо дедушке Морозу
-					</h1>
-				</div>
-				<div className='imagesWrapper'>
-					<div><img src={letterImg2} alt="Письмо деду морозу"/></div>
-					<div><img src={letterImg} alt="Письмо деду морозу"/></div>
-				</div>
-				<h1 className='title'>
-					Задание №1
-				</h1>
-				<p>
-					Расскажи дедушке о себе. Поблагодари дедушку Мороза за прошлый подарок.
-					Подумай, что бы ты хотел получить на Новый год, напиши свои мечты, а лучше
-					нарисуй. Вспомни, как ты провела этот год, что хорошего было сделано.
-				</p>
+	return (
+		<div className='container'>
+			<div className='numbersWrapper'>
+				<div><img src={timeImg} alt="Дата"/></div>
+				<div><img src={helloNumberImg} alt="Дата"/></div>
+			</div>
 
-			</div>);
+			<AudioPlayer/>
+
+			<div className="title">
+				{/*<PresentImg/>*/}
+				<h1>
+					Пишем письмо дедушке Морозу
+				</h1>
+			</div>
+			<div className='imagesWrapper'>
+				<div><img src={letterImg2} alt="Письмо деду морозу"/></div>
+				<div><img src={letterImg} alt="Письмо деду морозу"/></div>
+			</div>
+			<h1 className='title'>
+				Задание №1
+			</h1>
+			<p className='task'>
+				Расскажи дедушке о себе. Поблагодари дедушку Мороза за прошлый подарок.
+				Подумай, что бы ты хотел получить на Новый год, напиши свои мечты, а лучше
+				нарисуй. Вспомни, как ты провела этот год, что хорошего было сделано.
+			</p>
+
+		</div>);
 };
